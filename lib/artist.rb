672601a3c958deaafbcf5a4288be11b9144f9f23
Ryan McCahon <mccahonr@yahoo.com>
2020-05-20 22:19:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
-  include Paramable
   attr_accessor :name
   attr_reader :songs
 
